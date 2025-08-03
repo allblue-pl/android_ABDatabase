47787fb4f9faa3759d29f9f3f86081cd9d8ed7ac
AllBlue Monkey <j.zolcik@allblue.pl>
2025-08-03 11:40:11 +0200

    Development

--- abdatabase/build.gradle.kts
+++ abdatabase/build.gradle.kts
@@ -4,12 +4,13 @@ plugins {
 
 android {
     namespace = "pl.allblue.abdatabase"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
-        minSdk = 24
+        minSdk = 26
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
     }
 
     buildTypes {
